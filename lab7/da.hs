b31feb980657f13ad978ee9df2f63964ed670cb2
Oprea Lucian <47993986+Lucian-IoanOprea@users.noreply.github.com>
2020-07-10 12:22:59 +0300

    Add files via upload

---
+++ lab7/da.hs
@@ -0,0 +1,6 @@
+import Data.List
+import Data.Word
+import Data.Bits
+import Data.Char
+import System.Random hiding (randoms)
+
---
+++ lab7/legarea-var-functionale/my.hs
@@ -0,0 +1,10 @@
+import Data.List
+import Data.Word
+import Data.Bits
+import Data.Char
+import System.Random hiding (randoms)
+
+
+square x = x * x
+square2 x = (*) x x
+myx x = x + 100
